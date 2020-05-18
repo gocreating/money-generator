cb54c2b3bfff27e0b4835216b1ac515f4dbd61c5
gocreating <gocreating@gmail.com>
2020-05-18 22:54:34 +0800

    Remove old codebase

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  WS_HOST_PUBLIC: 'wss://api-pub.bitfinex.com/ws/2',
-  WS_HOST: 'wss://api.bitfinex.com/ws/2',
-};
--- sendAuthInput.js
+++
@@ -1,6 +0,0 @@
-const sendAuthInput = (wss, type, inputDetails) => {
-  const inputPayload = [0, type, null, inputDetails] // Note how the payload is constructed here. It consists of an array starting with the CHANNEL_ID, TYPE, and PLACEHOLDER and is followed by the inputDetails object.
-  wss.send(JSON.stringify(inputPayload)); // Submit payload for input
-};
-
-module.exports = sendAuthInput;
