c2d6ab0d1cbf3a924cf79794f3107c1301989771
gocreating <gocreating@gmail.com>
2020-05-21 21:31:59 +0800

    Refine order book styling

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'next/router';
+import styled, { css } from 'styled-components';
 import round from 'lodash/round';
 import useInterval from '../hooks/useInterval';
+
+const Table = styled.table`
+  border-collapse:collapse;
+`;
+
+const Th = styled.th`
+  ${props => props.alignRight && css`
+    text-align: right;
+  `}
+`;
+
+const Td = styled.td`
+  ${props => props.alignRight && css`
+    text-align: right;
+  `}
+  ${props => props.percentage && css`
+    position: relative;
+
+    ::after {
+      content: '';
+      width: ${props.percentage * 100}%;
+      display: block;
+      position: absolute;
+      top: 0px;
+      bottom: 0px;
+      z-index: -1;
+
+      ${props.percentageRed && css`
+        background-color: rgb(252, 220, 222);
+      `}
+      ${props.percentageGreen && css`
+        background-color: rgb(210, 235, 220);
+      `}
+      ${props.percentageRight ? css`
+        right: 0px;
+      ` : css`
+        left: 0px;
+      `}
+    }
+  `}
+`;
 
 const HomePage = ({ router }) => {
   const { BITFINEX_API_KEY, BITFINEX_API_SECRET } = router.query;
@@ -51,28 +93,31 @@ const HomePage = ({ router }) => {
 
   const balance = round(info.user.wallet?.funding.USD.balance || 0, 2);
   const balanceAvailable = round(info.user.wallet?.funding.USD.balanceAvailable || 0, 2);
+  const totalBidAmount = orderBook.bids.reduce((sum, bid) => sum - bid[3], 0);
+  const totalAskAmount = orderBook.asks.reduce((sum, ask) => sum + ask[3], 0);
+  let accBidAmount = 0;
+  let accAskAmount = 0;
 
   return (
     <div>
       <table>
+        <caption>Account</caption>
         <tbody>
           <tr>
-            <th>Dashboard Status</th>
-            <td>{status}</td>
+            <td>ID</td>
+            <td>{info.user.info?.id}</td>
           </tr>
           <tr>
-            <th>Refresh Interval</th>
-            <td>
-              {refreshSecond ? `Every ${refreshSecond} seconds` : 'waiting...'}
-            </td>
+            <td>email</td>
+            <td>{info.user.info?.email}</td>
           </tr>
           <tr>
-            <th>API Key</th>
-            <td>{BITFINEX_API_KEY}</td>
+            <td>username</td>
+            <td>{info.user.info?.username}</td>
           </tr>
           <tr>
-            <th>API Secret</th>
-            <td>{BITFINEX_API_SECRET}</td>
+            <td>timezone</td>
+            <td>{info.user.info?.timezone}</td>
           </tr>
           <tr>
             <th>Total Founding Balance</th>
@@ -81,15 +126,6 @@ const HomePage = ({ router }) => {
           <tr>
             <th>Available Founding Balance</th>
             <td>{`${balanceAvailable} USD (${round(balanceAvailable * 30, 0)} TWD)`}</td>
-          </tr>
-          <tr>
-            <th>User</th>
-            <td>
-              <div>{`id: ${info.user.info?.id}`}</div>
-              <div>{`email: ${info.user.info?.email}`}</div>
-              <div>{`username: ${info.user.info?.username}`}</div>
-              <div>{`timezone: ${info.user.info?.timezone}`}</div>
-            </td>
           </tr>
         </tbody>
       </table>
@@ -103,10 +139,10 @@ const HomePage = ({ router }) => {
             <th>Position Pair</th>
             <th>Status</th>
             <th>Type</th>
-            <th>Amount</th>
-            <th>Rate</th>
+            <Th alignRight>Amount</Th>
+            <Th alignRight>Rate</Th>
             <th>Period</th>
-            <th>Expires</th>
+            <Th alignRight>Expires</Th>
           </tr>
         </thead>
         <tbody>
@@ -128,10 +164,10 @@ const HomePage = ({ router }) => {
                 <td>{fc.positionPair}</td>
                 <td>{fc.status}</td>
                 <td>{fc.type}</td>
-                <td>{round(fc.amount, 2)}</td>
-                <td>{`${round(fc.rate * 100, 5)}% (${round(fc.rate * 365 * 100, 1)}% annualized)`}</td>
+                <Td alignRight>{round(fc.amount, 2)}</Td>
+                <Td alignRight>{`${round(fc.rate * 100, 5).toFixed(5)}% (${round(fc.rate * 365 * 100, 1).toFixed(1)}% annualized)`}</Td>
                 <td>{`${fc.period} days`}</td>
-                <td>{`in ${expStr}`}</td>
+                <Td alignRight>{`in ${expStr}`}</Td>
               </tr>
             );
           })}
@@ -165,7 +201,7 @@ const HomePage = ({ router }) => {
         </tbody>
       </table>
       <hr />
-      <table>
+      <Table>
         <caption>{'Order Book'}</caption>
         <thead>
           <tr>
@@ -174,39 +210,76 @@ const HomePage = ({ router }) => {
           </tr>
           <tr>
             <td>ID</td>
-            <td>Period</td>
-            <td>Amount</td>
-            <td>Rate</td>
+            <Td alignRight>Period</Td>
+            <Td alignRight>Amount</Td>
+            <Td alignRight>Rate</Td>
 
             <td>Rate</td>
             <td>Amount</td>
-            <td>Period</td>
+            <Td alignRight>Period</Td>
             <td>ID</td>
           </tr>
         </thead>
         <tbody>
           {orderBook.bids.map((bid, i) => {
             const ask = orderBook.asks[i];
+            accBidAmount -= bid[3];
+            accAskAmount += ask[3];
+
             return (
               <tr key={bid[0]}>
                 <td>{bid[0]}</td>
-                <td>{bid[1]}</td>
-                <td>{round(-bid[3], 1)}</td>
-                <td>
-                  {`${round(bid[2] * 100, 6)}%`}
+                <Td alignRight>{bid[1]}</Td>
+                <Td alignRight>
+                  {round(-bid[3], 1).toFixed(1)}
+                </Td>
+                <Td
+                  alignRight
+                  percentage={accBidAmount / (totalBidAmount + totalAskAmount)}
+                  percentageGreen
+                  percentageRight
+                >
                   {i === 0 && `(${round(bid[2] * 365 * 100, 1)}% annualized)`}
-                </td>
+                  {`${round(bid[2] * 100, 5).toFixed(5)}%`}
+                </Td>
 
-                <td>
-                  {`${round(ask[2] * 100, 6)}%`}
+                <Td
+                  percentage={accAskAmount / (totalBidAmount + totalAskAmount)}
+                  percentageRed
+                >
+                  {`${round(ask[2] * 100, 5).toFixed(5)}%`}
                   {i === 0 && `(${round(ask[2] * 365 * 100, 1)}% annualized)`}
-                </td>
-                <td>{round(ask[3], 1)}</td>
-                <td>{ask[1]}</td>
+                </Td>
+                <Td alignRight>{round(ask[3], 1).toFixed(1)}</Td>
+                <Td alignRight>{ask[1]}</Td>
                 <td>{ask[0]}</td>
               </tr>
             );
           })}
+        </tbody>
+      </Table>
+      <hr />
+      <table>
+        <caption>Monitor Dashboard</caption>
+        <tbody>
+          <tr>
+            <th>Bitfinex API Connection Status</th>
+            <td>{status}</td>
+          </tr>
+          <tr>
+            <th>Refresh Interval</th>
+            <td>
+              {refreshSecond ? `Every ${refreshSecond} seconds` : 'waiting...'}
+            </td>
+          </tr>
+          <tr>
+            <th>API Key</th>
+            <td>{BITFINEX_API_KEY}</td>
+          </tr>
+          <tr>
+            <th>API Secret</th>
+            <td>{BITFINEX_API_SECRET}</td>
+          </tr>
         </tbody>
       </table>
     </div>
